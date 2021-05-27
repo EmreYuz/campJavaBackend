a72b467abb3e218dd0f061e454a9d4d581f32b1e
emreyuz <emreyuz@gmail.com>
2021-05-27 23:08:51 +0300

    8. gün 2. ders sonu

--- src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -1,13 +1,19 @@
 package kodlamaio.northwind.api.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import kodlamaio.northwind.business.abstracts.ProductService;
 import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -25,7 +31,7 @@ public class ProductsController {
 
 
     @GetMapping("/getall")
-    public DataResult<List<Product>> getAll(){
+    public DataResult<List<Product>> getAll() {
         return this.productService.getAll();
     }
 
@@ -33,4 +39,11 @@ public class ProductsController {
     public Result add(@RequestBody Product product) {
         return this.productService.add(product);
     }
+
+    @GetMapping("/getByProductName")
+    public DataResult<Product> getByProductName(@RequestParam String productName) {
+        return this.productService.getByProductName(productName);
+    }
+
+
 }
--- src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -26,12 +26,50 @@ public class ProductManager implements ProductService {
     @Override
     public DataResult<List<Product>> getAll() {
 
-        return new SuccessDataResult<List<Product>>(this.productDao.findAll(), "Data listelendi.");
+        return new SuccessDataResult<List<Product>>(this.productDao.findAll(), "Veri listelendi.");
     }
 
     @Override
     public Result add(Product product) {
         this.productDao.save(product);
         return new SuccessResult("Ürün eklendi.");
+    }
+
+    @Override
+    public DataResult<Product> getByProductName(String productName) {
+        return new SuccessDataResult<Product>(this.productDao.getByProductName(productName), "Veri listelendi.");
+    }
+
+    @Override
+    public DataResult<Product> getByProductNameAndCategory(String productName, int categoryId) {
+
+        // iş kodları buraya yazılacak
+
+        return new SuccessDataResult<Product>(this.productDao.getByProductNameAndCategory(productName, categoryId), "Veri listelendi.");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByProductNameOrCategory(String productName, int categoryId) {
+        return new SuccessDataResult<List<Product>>(this.productDao.getByProductNameOrCategory(productName, categoryId), "Veri listelendi.");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByCategoryIn(List<Integer> categories) {
+        return new SuccessDataResult<List<Product>>(this.productDao.getByCategoryIn(categories), "Veri listelendi.");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByProductNameContains(String productName) {
+        return new SuccessDataResult<List<Product>>(this.productDao.getByProductNameContains(productName), "Veri listelendi.");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByProductNameStartsWith(String productName) {
+        return new SuccessDataResult<List<Product>>(this.productDao.getByProductNameStartsWith(productName), "Veri listelendi.");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId) {
+        return new SuccessDataResult<List<Product>>(this.productDao.getByNameAndCategory(productName, categoryId), "Veri listelendi.");
     }
 }
